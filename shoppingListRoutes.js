ff1c4aba1bf060ba9cefb21187a6dbe53a1e15f2
samau3 <samau415@gmail.com>
2021-09-29 16:31:06 -0700

    added patch and delete routes

--- shoppingListRoutes.js
+++ shoppingListRoutes.js
@@ -6,13 +6,24 @@ const router = new express.Router();
 const { NotFoundError } = require("./expressError");
 
 /** Find an item by name */
-function getItem(name){
-    for (item of db.items){
-        if(item.name === name){
+function getItem(name) {
+
+    for (item of db.items) {
+        if (item.name === name) {
             return item;
-        } 
+        }
     }
-    throw new NotFoundError()
+    throw new NotFoundError();
+}
+
+function itemIndex(name) {
+
+    for (let i = 0; i < db.items.length; i++) {
+        if (db.items[i].name === name) {
+            return i;
+        }
+    }
+    throw new NotFoundError();
 }
 
 /** GET /items : returns a list of all items: 
@@ -31,19 +42,44 @@ router.post("/", function (req, res) {
     let name = req.body.name;
     let price = req.body.price;
 
-    db.items.push({name, price});
+    db.items.push({ name, price });
 
-    return res.json({added: { name, price }});
+    return res.json({ added: { name, price } });
 });
 
 /** GET /items/:name : returns a single item by name: 
  *  { name: "name", price: # }
  */
 router.get("/:name", function (req, res) {
-    let reqName = req.body.name;
+    let reqName = req.params.name;
+
     let item = getItem(reqName);
 
-    return res.json( item );
+    return res.json(item);
 });
+
+router.patch("/:name", function (req, res) {
+    let reqName = req.params.name;
+    let newName = req.body.name;
+    let newPrice = req.body.price;
+
+    let item = getItem(reqName);
+    console.log("PATCH item", item)
+    item.name = newName;
+    item.price = newPrice;
+
+    return res.json({ updated: item });
+
+})
+
+router.delete("/:name", function (req, res) {
+    let reqName = req.params.name;
+    let index = itemIndex(reqName);
+
+    db.items.splice(index, 1);
+
+    return res.json({ message: "Deleted" });
+})
+
 
 module.exports = router;
